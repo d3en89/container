dbc128b3bafdd18427aee4dcf2d96c30be9488b1
d3en89 <stdenix889@yandex.ru>
2023-07-28 21:12:30 +0300

    отредактил speedtest

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,15 @@
 import sys
-import subprocess
+# import subprocess
 sys.path.insert(1, '/mnt/D2A44A41A44A2875/YandexDisk/dev/container')
 
 from webpage import app
-from static.script.get_speedtest import test_speed
+# from static.script.get_speedtest import test_speed
 
-try:
-    subprocess.check_call(["ping", "-c 1", "8.8.8.8"])
-    test_speed("ok")
-except subprocess.CalledProcessError:
-    test_speed("bad")
+# try:
+#     subprocess.check_call(["ping", "-c 1", "8.8.8.8"])
+#     test_speed("ok")
+# except subprocess.CalledProcessError:
+#     test_speed("bad")
 
 
 if __name__ == "__main__":
