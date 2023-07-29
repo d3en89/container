ef076e6632289f81c3faf1b403d7394f37873007
d3en89 <stdenix889@yandex.ru>
2023-07-29 13:41:56 +0300

    удалил лишние файлы

--- static/script/get_speedtest.py
+++ static/script/get_speedtest.py
@@ -5,22 +5,22 @@ import sys
 def test_speed(val):
 
     save_path = '/var/www/container/log/'
-    test_command = ['/usr/local/bin/speedtest-cli', '--secure']
-    start_script = subprocess.Popen(test_command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8')
+    #test_command = ['/usr/local/bin/speedtest-cli', '--secure']
+    start_script = subprocess.Popen('/usr/local/bin/speedtest-cli --secure', shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8')
 
     match val:
         case "ok":
             with open(f'{save_path}speed.log', 'w') as speed:
                 res = []
+                for line in start_script.stderr:
+                    if 'ERROR' in line:
+                        res += "" + line.strip()
                 for line in start_script.stdout:
-                    if 'ERROR' in line :
-                        speed.write(line)
-                        speed.close()
-                        break
                     if 'Download' in line or 'Upload' in line:
                         res.append(line.strip())
                 speed.write(" , ".join(res))
                 speed.close()
+                return res
         case "bad":
             with open(f'{save_path}speed.log', 'w') as speed:
                 speed.write("Not connection")
